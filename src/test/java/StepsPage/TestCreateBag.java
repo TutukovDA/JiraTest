44acc13a78bde16e5247ae200444f398fea8af59
TutukovDA <126241028+TutukovDA@users.noreply.github.com>
2023-05-11 14:35:34 +0400

    RealLastVersion

--- src/test/java/StepsPage/TestCreateBag.java
+++ src/test/java/StepsPage/TestCreateBag.java
@@ -14,12 +14,17 @@ public class TestCreateBag {
         BaseSelect("div","@id","issuetype-single-select","input").setValue("Ошибка").pressEnter();
         BaseInput("*","@class","text long-field").sendKeys(NAME_BUG);
         BaseInput("*","@id","description").sendKeys("Подробное описание ОШИБКИ");
+        BaseInput("*","@id","environment").sendKeys("Подробное описание Окуржения");
         BaseSelect("*","@id","fixVersions","optgroup/option[2]")
                 .shouldBe(Condition.visible, Duration.ofSeconds(20))
                 .click();
+        BaseButton("button","@id","assign-to-me-trigger").shouldBe(Condition.visible, Duration.ofSeconds(20)).click();
         BaseSelect("*","@id","versions","optgroup/option[2]")
                 .shouldBe(Condition.visible, Duration.ofSeconds(20))
                 .click();
+        BaseButton("textarea","@id","labels-textarea").sendKeys("tratat");
+        BaseButton("textarea","@id","issuelinks-issues-textarea").setValue("TEST-21967").pressEnter();
+        //BaseGetText("option","Доска Спринт 2").setValue("Доска Спринт 2").pressEnter();
         BaseButton("*","@id","create-issue-submit").shouldBe(Condition.visible, Duration.ofSeconds(20))
                 .click();
         BaseInput("input","@name","searchString").setValue(NAME_BUG).pressEnter();
@@ -27,5 +32,9 @@ public class TestCreateBag {
         Assert.assertTrue(BaseButton("img","@title","Ошибка").shouldBe(Condition.visible,Duration.ofSeconds(20)).exists());
         Assert.assertEquals("Version 2.0",BaseButton("span","@id","versions-val").shouldBe(Condition.visible,Duration.ofSeconds(20)).getText());
         Assert.assertNotNull(BaseInput("*","@id","description"));
+        Assert.assertNotNull(BaseInput("*","@id","environment"));
+        Assert.assertNotNull(BaseButton("textarea","@id","labels-textarea"));
+        Assert.assertNotNull(BaseButton("textarea","@id","issuelinks-issues-textarea"));
+
     }
 }
